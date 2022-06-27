0edfb7af0965f8fe67aa80c107562fcc52212c54
kreison <alankainazovr@gmail.com>
2022-06-27 17:52:16 +0600

    hw4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { changeTitleAction, postJsonPlaceholder } from "./action/actions";
 import MainPage from "./pages/mainPage/MainPage";
 
 function App() {
     const dispatch = useDispatch()
     const title = useSelector(state => state.title.title)
-    useEffect(() => {
-        dispatch(changeTitleAction())
-    }, [])
+   
 
 
     //hw
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
     const postResponse = () => {
-        dispatch(postJsonPlaceholder({name: 'Oleg', id: Date.now()}))
+        const obj = {
+            name: name,
+            email: email
+        }
+        dispatch(postJsonPlaceholder(obj))
     }
     return (
         <div className="App">
-            { title }
-            <MainPage />
+            <input value={name} onChange={(e)=> setName(e.target.value)} type="text" />
+            <input value={email} onChange={(e)=> setEmail(e.target.value)} type="email" />
             <button onClick={ postResponse }>send object</button>
         </div>
     );
